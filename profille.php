23ad57d432c5b8bdd1a8ddf7531b3b1e028ca2e3
mr anoubl <ahmed@gmail.com>
2023-02-27 21:14:35 +0100

    for reda

--- profille.php
+++ profille.php
@@ -7,6 +7,7 @@ $id=$_SESSION['id'];
 <!doctype html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
